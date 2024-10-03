2e19207df4a95b2cb38e0e5c8a093b39c5441075
ahmed <119008954+Ahmeddeco@users.noreply.github.com>
2024-10-03 20:28:06 +0300

    again

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
@@ -7,47 +7,13 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    fontFamily: {
-      expo: ['var(--font-expo-2)'],
-      expo_italic: ['var(--font-expo-2-italic)'],
-    },
-    container: {
-      padding: {
-        DEFAULT: '15px',
-      },
-    },
-    screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '960px',
-      xl: '1440px',
-    },
     extend: {
       colors: {
-        body: '#1D1F23',
-        primary: '#151618',
-        accent: {
-          DEFAULT: '#F6CD46',
-          hover: '#E1B72E',
-        },
-      },
-      backgroundImage: {
-        mainSlider: "url('img/mainSlider_bg.png')",
-      },
-      keyframes: {
-        shake: {
-          '10%, 90%': { transform: 'translate3d(-1px, 0,0 )' },
-          '20%, 80%': { transform: 'translate3d(2px, 0,0 )' },
-          '30%, 50%, 70%': { transform: 'translate3d(-4px, 0,0 )' },
-          '40%, 60%': { transform: 'translate3d(4px, 0,0 )' },
-        },
-      },
-      animation: {
-        shake: 'shake 1s ease-in-out',
+        background: "var(--background)",
+        foreground: "var(--foreground)",
       },
     },
   },
-
   plugins: [],
-}
-export default config
+};
+export default config;
