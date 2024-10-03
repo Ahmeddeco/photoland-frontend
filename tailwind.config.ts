8c8c2f9d00c471a04c21d25b5b95710d37be82db
ahmed <119008954+Ahmeddeco@users.noreply.github.com>
2024-10-03 21:03:05 +0300

    font and image

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
@@ -7,13 +7,51 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+    fontFamily: {
+      expo2: ['var(--font-expo2)'],
+      expo2_italic: ['var(--font-expo2_italic)'],
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '960px',
+      xl: '1440px',
+    },
+
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '15px',
       },
     },
+    extend: {
+      colors: {
+        body: '#1D1F23',
+        primary: '#151618',
+        accent: {
+          DEFAULT: '#F6CD46',
+          hover: '#E1B72E',
+        },
+
+      },
+    },
+    backgroundImage: {
+      mainSlider: "url('/mainSlider_bg.png')",
+    },
+    keyframes: {
+      shake: {
+        '10%, 90%': { transform: 'translate3d(-1px, 0,0 )' },
+        '20%, 80%': { transform: 'translate3d(2px, 0,0 )' },
+        '30%, 50%, 70%': { transform: 'translate3d(-4px, 0,0 )' },
+        '40%, 60%': { transform: 'translate3d(4px, 0,0 )' },
+      },
+    },
+    animation: {
+      shake: 'shake 1s ease-in-out',
+    },
   },
+
   plugins: [],
-};
-export default config;
+}
+
+export default config
