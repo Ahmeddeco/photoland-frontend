d321fc231f75c3bdfc5279912f45d229f60c836d
ahmed <119008954+Ahmeddeco@users.noreply.github.com>
2024-10-03 21:17:50 +0300

    components and pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function HomePage() {
 	return (
-		<main className='text-7xl font-bold capitalize flex items-center justify-center container h-screen '>
+		<main className='mainPage '>
 			Home Page
 		</main>
 	)
---
+++ app/products/page.tsx
@@ -0,0 +1,3 @@
+export default function ProductsPage() {
+	return <main className='mainPage'>Products Page</main>
+}
